129a42c403fe8bc9bcccb87acea3efa135e00fe6
Shane McKee <smckee6192@gmail.com>
2016-05-28 17:35:12 -0700

    fixed crawler only getting first page

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CS569FinalProject
+We're gonna crawl these repos like we're the freakin' Rugrats
--- crawler.py
+++ crawler.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
 import time, sys
-import notifier
+# import notifier
 from github import Github, GithubException
 from datetime import datetime
+import inspect
 
 # Configure with the appropriate user information before executing this script
-GITHUB_AUTH = ('username', 'password') # Github username and password (or token)
+GITHUB_AUTH = ('smckee6192', 'barefoot1') # Github username and password (or token)
 GMAIL_AUTH = ('username', 'password') # Gmail username and password (or token)
 NOTIFY = ['email_addr1', 'email_addr2'] # Email addresses to receive notifications
+SLEEP_TIME_SEC = 3600
 
 BANNED = [  'legacy-homebrew', 'gitignore', 'You-Dont-Know-JS', 'Font-Awesome',
             'free-programming-books', 'html5-boilerplate', 'the-art-of-command-line']
@@ -33,10 +35,18 @@ def run():
             repo_count += 1
             print("%d - project: %s" % (repo_count, repo.full_name))
             merges = 0
-            commits = [c.commit for c in repo.get_commits().get_page(0)]
-            for commit in commits:
+
+            print("Getting all commits from: %s" % repo.full_name)
+            allCommits = getAllCommits(repo)
+
+            for commit in allCommits:
+                # commitMembers = inspect.getmembers(commit.tree)
+                # for each in commit.tree.raw_data:
+                #     print each
+                # import pdb; pdb.set_trace()
                 parents = [parent.sha for parent in commit.parents]
-                if len(parents) > 1:
+                if len(parents) > 2:
+                    print commit.message
                     merges += 1
                     #print("\t%s %s %s" % (commit.message, commit.sha, commit.author.name))
             print("\tmerges: %d" % (merges))
@@ -44,18 +54,37 @@ def run():
             if repo_count % 10 == 0:
                 print "API rate remaining: %d" % github.get_rate_limit().rate.remaining
             if github.get_rate_limit().rate.remaining < 100:
-                print("Sleeping for %d seconds to replenish rate limit" % 3600)
-                time.sleep(3600) # sleep for 1 hour to allow rate limit to replenish
+                print("Sleeping for %d seconds to replenish rate limit" % SLEEP_TIME_SEC)
+                time.sleep(SLEEP_TIME_SEC) # sleep for 1 hour to allow rate limit to replenish
+
+            currentPage += 1
 
     except GithubException, exception:
         timestamp = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
         notice = "Github Exception received by crawler.py on %s.\nExamined %d repositories.\n\nException: %s" % (timestamp, repo_count, exception)
-        for recipient in NOTIFY:
-            notifier.send_notice(GMAIL_AUTH[0], GMAIL_AUTH[1], "CS566_FinalProject failure detected", recipient, notice)
+        # for recipient in NOTIFY:
+            # notifier.send_notice(GMAIL_AUTH[0], GMAIL_AUTH[1], "CS566_FinalProject failure detected", recipient, notice)
         print(notice)
     except:
         print("Unexpected error:", sys.exc_info()[0])
         raise
+
+def getAllCommits(repo):
+    numCommitsInPage = 30 #Pagination is 30 per page 
+    currentPage = 0
+    allCommits = []
+    
+    while numCommitsInPage > 0:
+        commits = [c.commit for c in repo.get_commits().get_page(currentPage)]
+        numCommitsInPage = len(commits)
+        allCommits += commits
+        sys.stdout.write("\rCommits Downloaded: %d" % len(allCommits))
+        sys.stdout.flush()
+
+        currentPage += 1
+
+    return allCommits
+
 
 if __name__ == "__main__":
     main()
