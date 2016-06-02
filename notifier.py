2d3cd220bad9ede985e1751917f62f4c35711c54
Nicholas Nelson <nelsonni@oregonstate.edu>
2016-06-02 05:59:13 -0700

    more explicit declarations in notifier

--- notifier.py
+++ notifier.py
@@ -3,6 +3,6 @@ from gmail.message import Message
 
 def send_notice(username, password, subject, to, cc, text):
     gmail = GMail(username, password)
-    msg = Message(subject, to, cc, text)
+    msg = Message(subject=subject, to=to, cc=cc, text=text)
     gmail.send(msg)
     gmail.close()
