b3bdd9e6d1ea69e89690fb0e979d7696b4da1645
Nicholas Nelson <nelsonni@oregonstate.edu>
2016-06-02 05:55:24 -0700

    fix for notifier to include cc: field

--- notifier.py
+++ notifier.py
@@ -1,8 +1,8 @@
 from gmail.gmail import GMail
 from gmail.message import Message
 
-def send_notice(username, password, subject, to, text):
+def send_notice(username, password, subject, to, cc, text):
     gmail = GMail(username, password)
-    msg = Message(subject, to, text)
+    msg = Message(subject, to, cc, text)
     gmail.send(msg)
     gmail.close()
