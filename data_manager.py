e6c7c9de8a468b751e2dd0e935ad41f0b4be5931
Nicholas Nelson <nelsonni@oregonstate.edu>
2016-06-08 01:11:12 -0700

    clean-up efforts

--- data_manager.py
+++ data_manager.py
@@ -10,6 +10,44 @@ def loadDictionaries(repo):
     
     return mergesDict, commitsDict
 
+def populateCommitsDict(repo, mDict, cDict):
+    """Populate commits dictionary of {SHA, git.Commit}
+
+    :param repo: git.repo.Repo instance containing current git repository.
+    :param mDict: Merge dictionary of {SHA, [parent1SHA, parent2SHA]}.
+    :param cDict: Commits dictionary of {SHA, git.Commit}
+    :return: Populated dictionary of {SHA, git.Commit}
+    """
+    branchesDict = {}
+    if DEBUG: print("Populating commitsDict...")
+    for merge in mDict:
+        print('Finding commit: %s' % merge)
+        commonBranches = sorted(branchesDict, key=branchesDict.get)
+        commonBranches.reverse()
+        branchName, commitObj = findCommitFromSHA(repo, merge, priorityBranches=commonBranches)
+
+        if commitObj != None:
+            cDict[merge] = commitObj
+            parents = mDict[merge]
+
+            if branchName in branchesDict:
+                branchesDict[branchName] += 1
+            else:
+                branchesDict[branchName] = 1    
+    return cDict
+
+def populateMergesDict(repo, mDict):
+# populates merge hash -> list of parent hashes dictionary
+# operates similar to 'git rev-list --merges --all' command
+    if DEBUG: print("Populating mergesDict...")
+    commitHashes = repo.git.rev_list(merges=True, all=True).split('\n')
+    for commitHash in commitHashes:
+        parentsString = repo.git.rev_list(commitHash, parents=True)
+        relevantLine = parentsString.split('\n')[0]
+        parents = relevantLine.split(' ')[1:]
+        mDict[str(commitHash)] = [str(x) for x in parents]
+    return mDict
+
 # repopulate commit hash -> commit object dictionary
 def repopulateCommitsDict(repo, bDict, cDict):
     if DEBUG: print("repopulating commitsDict...")
@@ -50,40 +88,6 @@ def findAllBranches(repo):
         if "origin/" in r.name:
             branchList.append(r.name)
     return branchList
-
-# populates commit hash -> commit object dictionary
-def populateCommitsDict(repo, mDict, cDict):
-    branchesDict = {}
-    if DEBUG: print("Populating commitsDict...")
-    for merge in mDict:
-        print('Finding commit: %s' % merge)
-        commonBranches = sorted(branchesDict, key=branchesDict.get)
-        commonBranches.reverse()
-        branchName, commitObj = findCommitFromSHA(repo, merge, priorityBranches=commonBranches)
-
-        if commitObj != None:
-            cDict[merge] = commitObj
-            parents = mDict[merge]
-
-            if branchName in branchesDict:
-                branchesDict[branchName] += 1
-            else:
-                branchesDict[branchName] = 1
-    
-    return cDict
-
-# populates merge hash -> list of parent hashes dictionary
-# operates similar to 'git rev-list --merges --all' command
-def populateMergesDict(repo, mDict):
-    if DEBUG: print("Populating mergesDict...")
-    commitHashes = repo.git.rev_list(merges=True, all=True).split('\n')
-    for commitHash in commitHashes:
-        parentsString = repo.git.rev_list(commitHash, parents=True)
-        relevantLine = parentsString.split('\n')[0]
-        parents = relevantLine.split(' ')[1:]
-        mDict[str(commitHash)] = [str(x) for x in parents]
-    return mDict
-
 
 def union(a, b):
     for each in b:
