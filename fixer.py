3c12834311ea1a21edef209c624e3737f420d710
Shane McKee <smckee6192@gmail.com>
2016-06-23 14:54:01 -0700

    Fixed the fixer to cover more complex cases

--- fixer.py
+++ fixer.py
@@ -1,4 +1,13 @@
+# except GitCommandError:
+#     log("GitCommandError: Your repo is probably in a bad state. Try:")
+#     log("git fetch origin")
+#     log("git reset --hard origin/master")
+#     log("git clean -d -f")
+#     log("git checkout master")
 
 def headcheck(repo):
     if repo.head.is_detached:
+        repo.git.fetch('origin')
+        repo.git.reset(['--hard', 'origin/master'])
+        repo.git.clean(['-d', '-f'])
         repo.git.checkout('master')
